6ab6b2647948878d138a0a9a1393096e861ee7be
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-03-26 18:35:36 +0300

    add css and minor refactor

--- script.js
+++ script.js
@@ -2,19 +2,13 @@
 function activate(index) {
     // get the images container
     var images_container = document.getElementById("images_container");
-    // get the chat container
-    var chat_container = document.getElementById("chat_container");
     // if index is 0
     if (index == 0) {
         // hide the images container
         images_container.style.display = "none";
-        // show the chat container
-        chat_container.style.display = "block";
     } else {
         // show the images container
         images_container.style.display = "block";
-        // hide the chat container
-        chat_container.style.display = "none";
     }
 }
 // a function to send the prompt value to the server using fetch
@@ -26,19 +20,19 @@ function send() {
     var chatInput = document.querySelector("input#chat").checked;
     var prompt = document.getElementById("prompt").value;
     let url = '', raw;
-    if (chatInput){
-        url = "https://api.openai.com/v1/completions";
+    if (chatInput) {
+        url = "https://api.openai.com/v1/chat/completions";
         raw = JSON.stringify({
-            "prompt": prompt,
-            "model": 'text-davinci-003',
-            "n": 2,
-        });
-    }else{
-         // get the number value from the input
-    var number = document.getElementById("number").value;
-    // get the size value from the input 
-    var size = document.getElementById("size").value;
-  
+            "model": "gpt-3.5-turbo",
+            "messages": [{ "role": "user", "content": prompt }]
+        }
+        );
+    } else {
+        // get the number value from the input
+        var number = document.getElementById("number").value;
+        // get the size value from the input 
+        var size = document.getElementById("size").value;
+
         url = "https://api.openai.com/v1/images/generations";
         raw = JSON.stringify({
             "prompt": prompt,
@@ -47,7 +41,7 @@ function send() {
         });
     }
     // get the prompt value
-     // change the send button text to sending
+    // change the send button text to sending
     document.getElementById("send").innerHTML = "Sending..";
     // disable the send button
     document.getElementById("send").disabled = true;
@@ -66,22 +60,30 @@ function send() {
     fetch(url, requestOptions)
         .then(response => response.json())
         .then(result => {
-            if (chatInput){
+            if (chatInput) {
                 // append inner html to chatlogs 
                 chatLogs.innerHTML += `<div class="user">User: ${prompt}</div>`
-                chatLogs.innerHTML += `<div class="chatGPT">ChatGPT: ${result.choices[0].text}</div>`
-            }else {
-            // clear the images div
-            chatLogs.innerHTML += `<div class="chatGPT">Dall-E: </div>`;
-            // loop through the result data
-            for (var i = 0; i < result.data.length; i++) {
-                // create a new image
-                var img = document.createElement("img");
-                // set the image source to the result data url
-                img.src = result.data[i].url;
-                // append the image to the images div
-                chatLogs.appendChild(img);
-            }
+                chatLogs.innerHTML += `<div class="chatGPT">ChatGPT: ${result.choices[0].message.content}</div>`
+            } else {
+        chatLogs.innerHTML += `<div class="user">User: provide an image to: ${prompt}</div>`
+
+                // clear the images div
+                var div = document.createElement("div");
+                div.className = "chatGPT";
+                div.innerHTML = "<div>Dall-E: </div>";
+                // loop through the result data
+                for (var i = 0; i < result.data.length; i++) {
+
+                    // create a new image
+                    var img = document.createElement("img");
+                    // set the image class to image
+                    img.className = "chatGPT";
+                    // set the image source to the result data url
+                    img.src = result.data[i].url;
+                    // append the image to the images div
+                    div.appendChild(img);
+                }
+                chatLogs.appendChild(div)
             }
             document.getElementById("send").innerHTML = "Send";
             document.getElementById("send").disabled = false;
