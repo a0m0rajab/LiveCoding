9bdccb8acfbbe24bb33294a44a03a4e204df348b
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-04-02 18:34:17 +0300

    add: change and update CSS

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function activate(index) {
         images_container.style.display = "none";
     } else {
         // show the images container
-        images_container.style.display = "block";
+        images_container.style.display = "flex";
     }
 }
 // a function to send the prompt value to the server using fetch
@@ -42,9 +42,9 @@ function send() {
     }
     // get the prompt value
     // change the send button text to sending
-    document.getElementById("send").innerHTML = "Sending..";
+    // document.getElementById("send").innerHTML = "Sending..";
     // disable the send button
-    document.getElementById("send").disabled = true;
+    // document.getElementById("send").disabled = true;
     // send chat: 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -57,36 +57,38 @@ function send() {
         redirect: 'follow'
     };
     let chatLogs = document.getElementById("chatlogs");
-    fetch(url, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            if (chatInput) {
-                // append inner html to chatlogs 
-                chatLogs.innerHTML += `<div class="user">User: ${prompt}</div>`
-                chatLogs.innerHTML += `<div class="chatGPT">ChatGPT: ${result.choices[0].message.content}</div>`
-            } else {
-        chatLogs.innerHTML += `<div class="user">User: provide an image to: ${prompt}</div>`
+    // fetch(url, requestOptions)
+    //     .then(response => response.json())
+    //     .then(result => {
+    //         if (chatInput) {
+    //             // append inner html to chatlogs 
+    //             chatLogs.innerHTML += `<div class="user">User: ${prompt}</div>`
+    //             chatLogs.innerHTML += `<div class="chatGPT">ChatGPT: ${result.choices[0].message.content}</div>`
+    //         } else {
+    //     chatLogs.innerHTML += `<div class="user">User: provide an image to: ${prompt}</div>`
 
-                // clear the images div
-                var div = document.createElement("div");
-                div.className = "chatGPT";
-                div.innerHTML = "<div>Dall-E: </div>";
-                // loop through the result data
-                for (var i = 0; i < result.data.length; i++) {
+    //             // clear the images div
+    //             var div = document.createElement("div");
+    //             div.className = "chatGPT";
+    //             div.innerHTML = "<div>Dall-E: </div>";
+    //             // loop through the result data
+    //             for (var i = 0; i < result.data.length; i++) {
 
-                    // create a new image
-                    var img = document.createElement("img");
-                    // set the image class to image
-                    img.className = "chatGPT";
-                    // set the image source to the result data url
-                    img.src = result.data[i].url;
-                    // append the image to the images div
-                    div.appendChild(img);
-                }
-                chatLogs.appendChild(div)
-            }
-            document.getElementById("send").innerHTML = "Send";
-            document.getElementById("send").disabled = false;
-        })
-        .catch(error => console.log('error', error));
+    //                 // create a new image
+    //                 var img = document.createElement("img");
+    //                 // set the image class to image
+    //                 img.className = "chatGPT";
+    //                 // set the image source to the result data url
+    //                 img.src = result.data[i].url;
+    //                 // append the image to the images div
+    //                 div.appendChild(img);
+    //             }
+    //             chatLogs.appendChild(div)
+    //         }
+    //         document.getElementById("send").innerHTML = "Send";
+    //         document.getElementById("send").disabled = false;
+    //     })
+    //     .catch(error => console.log('error', error));
+    chatLogs.innerHTML += `<div class="user">User: ${prompt}</div>`
+    chatLogs.innerHTML += `<div class="chatGPT">ChatGPT: ChatGPT Answer</div>`
 }
